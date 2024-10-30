0c0afa2b635e1012213411eb30e0a076c171f31b
JeanIfsp <jean.marciel@aluno.ifsp.edu.br>
2024-10-29 22:38:55 -0300

    Restructured function schedule

---
+++ src/barbershop/schedule/service.py
@@ -0,0 +1,51 @@
+from barbershop.models import Schedules, Service
+from barbershop.choices import  WeekType
+
+class ShedulesService:
+    
+    def get_list_shedules(self) -> Schedules:
+
+        return Schedules.objects.all()
+    
+    def get_list_services_and_prices(self):
+
+        return Service.objects.all()
+    
+    def get_days_not_registers(self):
+   
+        day_register = Schedules.objects.values_list('day', flat=True)
+        portuguese_days = [day.label.upper() for day in WeekType]
+        day_that_is_not_on_the_schedule = (set(portuguese_days) - set(list(day_register)))
+        return list(day_that_is_not_on_the_schedule)
+    
+    def get_hours_day(self, id):
+
+        return Schedules.objects.get(id=id)
+    @staticmethod
+    def get_hours_by_day_name_week(day):
+
+        return Schedules.objects.get(day__iexact=day)
+    
+    def create_new_scheduler(self, data):
+
+        day = data.POST.get('day')
+        start_work = data.POST.get('start_work')
+        end_work = data.POST.get('end_work')
+        launch_time = data.POST.get('launch_time')
+
+        schedules = Schedules.objects.create(day=day, start_time=start_work, end_time=end_work, launch_time=int(launch_time))
+        schedules.save()
+    
+    def update_schedule(self, instance, data):
+
+        start_work = data.POST.get('start_work')
+        end_work = data.POST.get('end_work')
+        instance.start_time=start_work
+        instance.end_time=end_work
+        instance.save()
+
+    def delete_schedule(self, id):
+        
+        instance = self.get_hours_day(id)
+        instance.delete()
+    
---
+++ src/barbershop/schedule/urls_schedule.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from barbershop.schedule import schedule_views
+
+urlpatterns = [   
+    path('list_schedule', schedule_views.list_schedule, name='list_schedule'),
+    path('register_schedule', schedule_views.register_schedule, name='register_schedule'),
+    path('update_schedule/<int:id>', schedule_views.update_schedule, name='update_schedule'),
+    path('delete_schedule/<int:id>', schedule_views.delete_schedule, name='delete_schedule'),
+]
