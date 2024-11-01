9cdd5313f515ed8a132e6cc37092b7a53bd7d8b0
JeanIfsp <jean.marciel@aluno.ifsp.edu.br>
2024-10-31 22:48:28 -0300

    Created new validate

--- src/barbershop/validator.py
+++ src/barbershop/validator.py
@@ -8,13 +8,15 @@ def available_day(request):
 
     if request.POST.get('hours') == None:
         raise ValidationException(f"Não temos horários disponivel para este dia")
-    
+    print(request.POST.get('date_time'))
+    print(request.POST.get('hours'))
+
     date_time_str = request.POST.get('date_time') + "T" + request.POST.get('hours')
-  
+    print(date_time_str)
     new_appointment_time = datetime.strptime(date_time_str, '%Y-%m-%dT%H:%M')
    
     service_appointment = ServiceAppointment()
-    exits_datetime = service_appointment.deltatime_created_new_appointmnet(new_appointment_time)
+    exits_datetime = service_appointment.get_hours_has_status_canceled(new_appointment_time)
 
     if exits_datetime:
         raise ValidationException(f"Horários indisponível {new_appointment_time}, agende outro horário")
