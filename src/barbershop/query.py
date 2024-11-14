663dc1fa21d75ef1fe7b7454e459afd9ad2ec0b1
JeanIfsp <jean.marciel@aluno.ifsp.edu.br>
2024-11-13 22:52:41 -0300

    Create service to class BarbershopService

--- src/barbershop/query.py
+++ src/barbershop/query.py
@@ -18,6 +18,12 @@ class BarbershopService:
         client_names = ClientBarbershop.objects.filter(admin_id=user).values('client__id','client__client_name')
         return client_names
     
+    
     def get_client_list_by_admin_barbeshop(admin):
         clients_ids = ClientBarbershop.objects.filter(admin_id=admin).values_list('client_id', flat=True)
         return clients_ids
+    
+    @staticmethod
+    def get_data_barbershop():
+        barbershops = Barbershop.objects.all()
+        return barbershops
