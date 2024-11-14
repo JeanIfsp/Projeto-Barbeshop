89cb0cbae17a27a967137d5a7b6ef252d698fba7
JeanIfsp <jean.marciel@aluno.ifsp.edu.br>
2024-11-13 22:46:07 -0300

    Removed print

--- src/barbershop/permissions.py
+++ src/barbershop/permissions.py
@@ -4,7 +4,6 @@ from functools import wraps
 def admin_required(view_func):
     @wraps(view_func)  # Preserva o nome e docstring da função original
     def _wrapped_view(request, *args, **kwargs):
-        print(request.user.user_type)
         if request.user.is_authenticated and request.user.user_type == 'ADMIN':
             return view_func(request, *args, **kwargs)
         else:
